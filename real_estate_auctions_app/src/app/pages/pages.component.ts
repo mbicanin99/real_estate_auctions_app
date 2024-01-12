cab731f73487bbd8fd37cb4bf75ef388fb842b09
mbicanin99 <mbicanin99f@gmail.com>
2024-01-12 14:46:53 +0100

    Update pages stylet and resolve bug

--- real_estate_auctions_app/src/app/pages/pages.component.ts
+++ real_estate_auctions_app/src/app/pages/pages.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
      selector: 'pages-component',
      templateUrl: './pages.component.html',
+     styleUrls: ['./pages.component.scss']
 })
 
 export class PagesComponent{
