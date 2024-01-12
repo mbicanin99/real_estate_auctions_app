ee91b0a9e62386eeb51d516f966971841bc4952f
mbicanin99 <mbicanin99f@gmail.com>
2024-01-12 14:44:20 +0100

    Update login component and use input validation directive

--- real_estate_auctions_app/src/app/auth/login-page/login.component.ts
+++ real_estate_auctions_app/src/app/auth/login-page/login.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { Store} from '@ngrx/store';
-import { FormBuilder, FormGroup} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginActions } from './store/actions/login.actions';
 
 @Component({
@@ -8,22 +8,63 @@ import { LoginActions } from './store/actions/login.actions';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginPageComponent{
+export class LoginPageComponent implements OnInit {
+  userNotExists:boolean = true;
 
-  constructor(private store: Store, private fb: FormBuilder){}
+  constructor(private store: Store, private fb: FormBuilder) {}
 
   loginForm: FormGroup = this.fb.group({
-    email: ['', {
-      updateOn: 'change',
-    }],
-    password: ['',{
-      updateOn: 'change',
-    }]
-  })
-  
+    email: [
+      '',
+      {
+        updateOn: 'change',
+      },
+    ],
+    password: [
+      '',
+      {
+        updateOn: 'change',
+      },
+    ],
+  });
+
+  // login() {
+  //   this.store.dispatch(LoginActions.confirm({email: this.loginForm.value['email'], password: this.loginForm.value['password']}));
+  //  }
+
   login() {
-    this.store.dispatch(LoginActions.confirm({email: this.loginForm.value['email'], password: this.loginForm.value['password']}));
-   }
+    if (this.loginForm.valid) {
+      this.store.dispatch(
+        LoginActions.confirm({
+          email: this.loginForm.value['email'],
+          password: this.loginForm.value['password'],
+        })
+      );
+    } else {
+      Object.keys(this.loginForm.controls).forEach((key) => {
+        const control = this.loginForm.get(key);
+        if (control.invalid) {
+          control.markAsTouched(); //to provide error
+        }
+      });
+    }
+    const storedUserString = localStorage.getItem('user');
+    const storedUser = JSON.parse(storedUserString);
+    
+    if(storedUser===null){
+      this.userNotExists=false;
+      console.log("Ovaj user ne postoji!")
+    }else{
+      console.log("Bravo postoji")
+    }
+    
+  }
 
 
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
 }
