c89b10a3c618d6bc86d5a8fac7ba9b3c98b76d8b
mbicanin99 <mbicanin99f@gmail.com>
2024-01-12 14:52:51 +0100

    Update getRealEstatePhoto() function in api service

--- real_estate_auctions_app/src/app/api-services/api.service.ts
+++ real_estate_auctions_app/src/app/api-services/api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { AppImage } from '../utils/types/image.interface';
+import { query } from 'express';
 
 @Injectable({
   providedIn: 'root'
@@ -10,38 +11,47 @@ export class ApiService {
   private accessKey = 'PKn0SKikVvOfWJ8HythVdupXfby31gdhNST8bU7d8Yc';
   private apiUrl = 'https://api.unsplash.com';
 
+  public data=[];
+  private url = 'https://api.unsplash.com/search/photos?client_id=PKn0SKikVvOfWJ8HythVdupXfby31gdhNST8bU7d8Yc&query=';
+
   constructor(private http: HttpClient) { }
 
-  // getRealEstatePhoto(numberOfRealEstates: number): Observable<any> {
-  //   const endpoint = '/photos'; // Unsplash endpoint za dohvat slika
-  //   const query = 'real estate'; // Pretraga slika sa određenim pojmom
-  //   const perPage = numberOfRealEstates; // Broj slika koje želite dohvatiti
-
-  //   return this.http.get(`${this.apiUrl}${endpoint}?query=${query}&per_page=${perPage}&client_id=${this.accessKey}`);
-  // }
-  // getRealEstatePhoto(numberOfRealEstates: number): Observable<AppImage[]> {
-  //   const endpoint = '/photos';
-  //   const query = 'real estate';
-  //   const perPage = numberOfRealEstates;
-
-  //   return this.http.get<any[]>(`${this.apiUrl}${endpoint}?query=${query}&per_page=${perPage}&client_id=${this.accessKey}`).pipe(
-  //     map(response => response.map(item => ({
-  //       id: item.id,
-  //       url: item.urls?.full,
-  //     })))
-  //   );
-  // }
 
   getRealEstatePhoto(numberOfRealEstates: number): Observable<AppImage[]> {
     const endpoint = '/photos';
     const keywords = 'house apartment real estate property';
     const perPage = numberOfRealEstates;
+    const query = 'real estate property';
   
-    return this.http.get<any[]>(`${this.apiUrl}${endpoint}?query=${keywords}&per_page=${perPage}&client_id=${this.accessKey}`).pipe(
-      map(response => response.map(item => ({
+    // return this.http.get<any[]>(`${this.apiUrl}${endpoint}?query=${query}&per_page=${perPage}&client_id=${this.accessKey}`).pipe(
+    //   map(response => response.map(item => ({
+    //     id: item.id,
+    //     url: item.urls?.full,
+    //   })))
+    // );
+
+    return this.http.get<any[]>(`${this.url + query}&per_page=${perPage}&client_id=${this.accessKey}`).pipe(
+      map((response: any) => response.results.map(item => ({  
         id: item.id,
         url: item.urls?.full,
       })))
     );
   }
+
+
+  getImage(numberOfRealEstates: number): any {
+    const query1 = 'real estate property';
+    this.http.get(this.url + query1).subscribe(
+      (res: any) => {
+        this.data = res.results.map(item => ({
+          id: item.id,
+          url: item.urls.full,
+        }));
+      }
+    );
+  
+    return this.data;
+  }
+
+
 }
